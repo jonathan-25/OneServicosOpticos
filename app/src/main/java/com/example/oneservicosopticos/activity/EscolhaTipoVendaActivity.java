c03da0040f91cbffc0ba1aed1088cce5b42f06bd
jonathan-25 <jbmm25@gmail.com>
2025-02-03 23:30:56 -0300

    ajuste simples ui

--- app/src/main/java/com/example/oneservicosopticos/activity/EscolhaTipoVendaActivity.java
+++ app/src/main/java/com/example/oneservicosopticos/activity/EscolhaTipoVendaActivity.java
@@ -10,9 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.oneservicosopticos.EscolhaLentesActivity;
 import com.example.oneservicosopticos.R;
-import com.example.oneservicosopticos.TelaInicialActivity;
 
 public class EscolhaTipoVendaActivity extends AppCompatActivity {
 
